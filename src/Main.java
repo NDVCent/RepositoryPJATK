44d621e20899236b5a5a873d7d88ede120d65f33
NDVCent <s30474@pjwstk.edu.pl>
2024-10-13 22:01:53 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1,2));
+
+        Subtractor substractor = new Substractor();
+        System.out.println(substractor.subtract(1,2));
+    }
+}
